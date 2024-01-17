f8f98243024ab41c46eb01edf34049dfcfe32d71
Natali Tremasova <n.resova@yandex.ru>
2024-01-17 23:11:17 +0300

    add lesson3: task3

---
+++ lesson3/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintSquares(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+//вызов функции
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
---
+++ lesson3/task3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Заменить четные элементы массива нулями
+
+void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++) 
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+//создаем целочисленный массив
+int[] array = { 2, 10, 8, 7, 4, 5};
+//запускаем функцию, в которую передадим наш массив
+ZeroEvenElements(array);/* тип данных не указываем при вызове функции*/
+PrintArray(array);
