850eb0f5c4a3371955ad5473d2a2e37363647e20
Natali Tremasova <n.resova@yandex.ru>
2024-01-29 21:50:51 +0300

    add lesson3: functions

--- lesson3/task3/Program.cs
+++ lesson3/task3/Program.cs
@@ -1,4 +1,4 @@
-﻿//Заменить четные элементы массива нулями
+﻿/*//Заменить четные элементы массива нулями
 
 void ZeroEvenElements(int[] arr)
 {
@@ -20,5 +20,66 @@ void PrintArray(int[] arr)
 //создаем целочисленный массив
 int[] array = { 2, 10, 8, 7, 4, 5};
 //запускаем функцию, в которую передадим наш массив
-ZeroEvenElements(array);/* тип данных не указываем при вызове функции*/
+ZeroEvenElements(array);/* тип данных не указываем при вызове функции
 PrintArray(array);
+*/
+/*
+// Генерация случайных чисел
+Random rnd = new Random();
+
+int size = 1000;
+int[] arr_int = new int[size];
+int i = 0;
+while (i < size)
+{
+    arr_int[i] = rnd.Next(1, 10);
+    i = i + 1;
+}
+
+i = 0;
+while (i < size)
+{
+    Console.Write($"{arr_int[i]} ");
+    i = i + 1;
+}
+*/
+/*
+//Введение данных с консоли
+
+//Console.WriteLine("Введите целое число:");
+
+//string input = Console.ReadLine();//считывание строки с консоли и сохранение в переменной input
+//int value = Convert.ToInt32(input);// преобразование считанных данных из строкового типа string в целый int
+
+int size = 10;
+int[] arr_int = new int[size];
+int i = 0;
+while (i < size)
+{
+    Console.WriteLine("Введите элемент массива(целое число):");
+    string input = Console.ReadLine();
+    arr_int[i] = Convert.ToInt32(input);
+    i = i + 1;
+}
+
+i = 0;
+while (i < size)
+{
+    Console.Write($"{arr_int[i]} ");
+    i = i + 1;
+}
+*/
+
+// Вывод 
+void PrintArray(int[] arr);
+{
+    foreach(int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+void ShowMessage(string msg)
+{
+    Console.Write(msg);
+}
